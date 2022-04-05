8bca5fc72d6dda8df1923caf00cc3b9a1e9edae1
Abubakir Kais <bakir_janabi@yahoo.com>
2022-04-06 00:52:08 +0400

    Adjusted the models

--- segmentation/models.go
+++ segmentation/models.go
@@ -1,12 +1,16 @@
 package segmentation
 
 type Segment struct {
-	Level    int
-	Equation string
+	Level         int
+	Equation      string
+	Tokens        []Token
+	ComputedValue float64
 }
 
 type Token struct {
-	Number string
-	Sign   string
-	Order  int
+	Number         string
+	Sign           string
+	Order          int
+	InsideBrackets bool
+	ComputedValue  float64
 }
